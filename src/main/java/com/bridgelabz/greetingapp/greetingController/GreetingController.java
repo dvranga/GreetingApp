4816396d25fc762fc0e0fc228fc67e4bcb201883
dvranga <dvranga@users.noreply.github.com>
2020-12-18 08:00:10 +0530

    [ranganath]Added:Using greeting controller return json for different http methods

--- src/main/java/com/bridgelabz/greetingapp/greetingController/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/greetingController/GreetingController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.greetingapp.greetingController;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +19,14 @@ public class GreetingController {
 		return new Greeting(counter.incrementAndGet(),
 				String.format(template, name));
 	}
+	
+	@GetMapping("/greeting/{name}")
+	public Greeting greeting1(@PathVariable(value = "name") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
 }
+
+
+
+
